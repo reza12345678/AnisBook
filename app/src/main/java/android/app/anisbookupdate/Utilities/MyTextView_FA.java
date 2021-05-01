2429c71486df22ac7ca15e1e4fe113506465423e
reza <reza.abdykian@gmail.com>
2021-05-01 10:20:19 +0430

    create SettingActivity and TextView_FA in Utility.java class and etc

---
+++ app/src/main/java/android/app/anisbookupdate/Utilities/MyTextView_FA.java
@@ -0,0 +1,33 @@
+package android.app.anisbookupdate.Utilities;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatTextView;
+
+import com.orhanobut.hawk.Hawk;
+
+public class MyTextView_FA extends AppCompatTextView {
+    public MyTextView_FA(@NonNull Context context) {
+        super(context);
+        setTypeface(Utility.getTypeFace_FA());
+        setTextSize(Hawk.get("font_size_FA"));
+    }
+
+    public MyTextView_FA(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setTypeface(Utility.getTypeFace_FA());
+        setTextSize(Hawk.get("font_size_FA"));
+    }
+
+    public MyTextView_FA(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setTypeface(Utility.getTypeFace_FA());
+        setTextSize(Hawk.get("font_size_FA"));
+    }
+}
+
+
+//
