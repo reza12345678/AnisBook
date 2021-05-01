f2696f46153e2160a616a25ccfae0a514e8da335
reza <reza.abdykian@gmail.com>
2021-05-01 10:13:43 +0430

    create SettingActivity and TextView_AB in Utility.java class and etc

--- app/src/main/java/android/app/anisbookupdate/MainActivity.java
+++ app/src/main/java/android/app/anisbookupdate/MainActivity.java
@@ -1,14 +1,124 @@
 package android.app.anisbookupdate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.anisbookupdate.Utilities.MyApplication;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseSequence;
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseView;
+import uk.co.deanwild.materialshowcaseview.ShowcaseConfig;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    DrawerLayout drawer;
+
+    ImageButton btnToggle;
+    Button btnContents, btnSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bind();
+        showcaseToggle();
+    }
+
+    private void showcaseToggle() {
+        new MaterialShowcaseView.Builder(this)
+                .setTarget(btnToggle)
+                .setDismissText("متوجه شدم")
+                .setContentText("برای دیدن فهرست مطالب و تنظیمات برنامه بر روی این دکمه کلیک کنید")
+                .setDelay(1000) // optional but starting animations immediately in onCreate can make them choppy
+                .singleUse("IDOne") // provide a unique ID used to ensure it is only shown once
+                .setGravity(Gravity.CENTER)
+                .show();
+    }
+
+    private void showcasepanel() {
+
+        // sequence example
+        ShowcaseConfig config = new ShowcaseConfig();
+        config.setDelay(500); // half second between each showcase view
+        config.setDismissTextColor(Color.RED);
+
+
+        MaterialShowcaseSequence sequence = new MaterialShowcaseSequence(this, "IDTwO");
+
+        sequence.setConfig(config);
+
+        sequence.addSequenceItem(btnContents,
+                "برای نمایش فهرست مطالب بر روی این دکمه کلیک کنید", "متوجه شدم");
+
+        sequence.addSequenceItem(btnSetting,
+                "برای تنظیمات برنامه بر روی این دکمه کلیک کنید", "متوجه شدم");
+
+        sequence.start();
+
+    }
+
+    private void bind() {
+        drawer = findViewById(R.id.drawerLay);
+
+        btnToggle = findViewById(R.id.btnToggle);
+        btnToggle.setOnClickListener(this);
+
+        btnContents = findViewById(R.id.btnContents);
+        btnContents.setOnClickListener(this);
+
+        btnSetting = findViewById(R.id.btnSetting);
+        btnSetting.setOnClickListener(this);
+
+        findViewById(R.id.btnCloseApp).setOnClickListener(this);
+        findViewById(R.id.btnSeenLast).setOnClickListener(this);
+        findViewById(R.id.btnRelateUs).setOnClickListener(this);
+        findViewById(R.id.btnSearch).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnCloseApp) {
+            this.finish();
+            System.exit(0);
+        }
+
+        if (v.getId() == R.id.btnToggle) {
+            if (!drawer.isDrawerOpen(Gravity.RIGHT))
+                drawer.openDrawer(Gravity.RIGHT);
+            showcasepanel();
+        }
+
+/*        if (v.getId() == R.id.btnContents) {
+            Intent actContents = new Intent(MyApplication.mContex, fstContentsActivity.class);
+            startActivity(actContents);
+        }
+
+        if (v.getId() == R.id.btnSetting) {
+            Intent actSetting = new Intent(MyApplication.mContex, SettingActivity.class);
+            startActivity(actSetting);
+        }
+
+        if (v.getId() == R.id.btnRelateUs) {
+            Intent actAbout = new Intent(MyApplication.mContex, RelateUsActivity.class);
+            startActivity(actAbout);
+        }
+
+        if (v.getId() == R.id.btnSeenLast) {
+        }
+
+        if (v.getId() == R.id.btnSearch) {
+            Intent actSearch = new Intent(MainActivity.this, SearchingActivity.class);
+            startActivity(actSearch);
+        }*/
+
     }
 }
---
+++ app/src/main/java/android/app/anisbookupdate/Utilities/MyTextView_AB.java
@@ -0,0 +1,30 @@
+package android.app.anisbookupdate.Utilities;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatTextView;
+
+import com.orhanobut.hawk.Hawk;
+
+public class MyTextView_AB extends AppCompatTextView {
+    public MyTextView_AB(@NonNull Context context) {
+        super(context);
+        setTypeface(Utility.getTypeFace_AB());
+        setTextSize(Hawk.get("font_size_AB"));
+    }
+
+    public MyTextView_AB(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setTypeface(Utility.getTypeFace_AB());
+        setTextSize(Hawk.get("font_size_AB"));
+    }
+
+    public MyTextView_AB(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setTypeface(Utility.getTypeFace_AB());
+        setTextSize(Hawk.get("font_size_AB"));
+    }
+}
