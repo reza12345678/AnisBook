33f81f4bffd47518297e5adaafb51e53acaa0f23
reza <reza.abdykian@gmail.com>
2021-05-05 09:58:27 +0430

    create RelatedActivity and change it in MainActivity and etc

---
+++ app/src/main/java/android/app/anisbookupdate/RelatedActivity.java
@@ -0,0 +1,14 @@
+package android.app.anisbookupdate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelatedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_related);
+    }
+}
